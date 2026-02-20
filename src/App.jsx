e771c27e530a4b7182f9c14eab1e33fd4d70fd76
EnergyOG <dennispeprah34@gmail.com>
2026-02-20 20:21:28 +0000

    Randy's Decor Init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App(){
+  return <>
+    <div className="bg-gray-600 w-[100%] h-[100vh] ">
+      <h1 className="text-sky-200 font-monospace text-2xl">Randy's decor</h1>
+    </div>
+  </>
+}
+
+export default App;
